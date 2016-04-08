e1c4ff2bc3025ba18fc18078282c747a698d8461
Nikola Milinkovic <nikola1981@gmail.com>
2016-04-08 14:06:48 +0200

    Generated with CORS filter.

---
+++ backend/src/main/java/todo/backend/config/WebConfiguration.java
@@ -0,0 +1,41 @@
+package todo.backend.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+public class WebConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(WebConfiguration.class);
+
+    @Bean
+    @Profile("dev")
+    public CorsFilter corsFilter() {
+
+        log.info("Initializing CORS filter...");
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("OPTIONS");
+        config.setMaxAge(1800L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", config);
+
+        return new CorsFilter(source);
+    }
+
+}
